f45dd728d5a8054236dcca596ec688fa999c3c6b
mattyhempstead <matty.hempstead@gmail.com>
2021-05-17 15:46:39 +1000

    Add tinywm boilerplate
    not sure how clean ima keep this git history

---
+++ README.md
@@ -0,0 +1,7 @@
+# WiM
+
+imagine being so pedantic that you decide to write your own window manager
+
+
+ty tinywm for the boilerplate
+
---
+++ src/start.sh
@@ -0,0 +1,15 @@
+# Create application containing entire X display for testing
+Xephyr -ac -screen 1280x720 -br -reset -terminate 2> /dev/null :1 &
+DISPLAY=:1
+
+# Gotta wait a bit before running wm idk why
+sleep 1
+
+echo Starting tinywm
+
+# start a terminal
+# as this is on display :1, it will appear in tinywm
+urxvt &
+
+../build/tinywm
+
